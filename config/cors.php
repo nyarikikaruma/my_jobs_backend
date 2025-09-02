3d94f94ca44ed1ed7e68e8c66895e1284a4e81c3
nyarikikaruma <nyarikikaruma@gmail.com>
2025-09-02 20:15:15 +0300

    Live url added to cors list updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://my-jobs-frontend.vercel.app/'], // Your Nuxt dev server
+    'allowed_origins' => ['http://localhost:3000', 'https://my-jobs-frontend.vercel.app'], // Your Nuxt dev server
 
     'allowed_origins_patterns' => [],
 
